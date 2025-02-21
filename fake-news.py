b82ed31d91648765869013319a158c67ed2afcde
rutujajain <121030753+itsrutuja123@users.noreply.github.com>
2025-02-21 09:55:30 +0530

    Update fake-news.py

--- fake-news.py
+++ fake-news.py
@@ -1,4 +1,5 @@
-# Import necessary libraries
+# Import libraries
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
